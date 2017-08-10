29a6af9185de02008c98cc34939cbaddf00582d7
The Transmogrifox <transmogrifox@cackleberrypines.net>
2017-08-10 12:07:23 -0700

    Update inductorwah.cpp
    Add approximation to the 1/x function in gp warping function

--- src/inductorwah.cpp
+++ src/inductorwah.cpp
@@ -326,7 +326,8 @@ iwah_tick(float x, float gp, iwah_coeffs* cf)
     if(gp < 0.0) gp = 0.0;
     
     //The magic numbers below approximate frequency warping characteristic
-    float gw = 4.6-18.4/(4.0 + gp);
+    //float gw = 4.6-18.4/(4.0 + gp);
+    float gw = gp * (1.15 - 0.235*gp); //Approximation of the approximation
                             
     //Update Biquad coefficients
     cf->a0 = cf->a0b + gw*cf->a0c;
